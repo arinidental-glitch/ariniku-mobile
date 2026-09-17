75d11acf843015ba1f79efd35b257037cd7f21ff
Ilham Mughni <ilhamgn@gmail.com>
2026-09-17 15:34:02 +0700

    Upgrade Expo SDK 51

--- android/app/src/main/java/com/adc/MainApplication.java
+++ android/app/src/main/java/com/adc/MainApplication.java
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false;
+    
     }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
